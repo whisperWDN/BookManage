1bf0267d652a17bddc719da0d8a8fea67a3c6ab2
whisperWDN <747618283@qq.com>
2022-06-15 22:59:43 +0800

    完成必做功能一

--- app/src/main/java/com/example/bookmanage/AddBookActivity.java
+++ app/src/main/java/com/example/bookmanage/AddBookActivity.java
@@ -7,13 +7,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bookmanage.http.HttpRequestUtil;
+import com.example.bookmanage.http.tool.HttpReqData;
+import com.example.bookmanage.http.tool.HttpRespData;
+
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +32,10 @@ public class AddBookActivity extends AppCompatActivity {
         Edit_ISBN.addTextChangedListener(new TheTextWatcher(Edit_ISBN));
         Button searchBook = findViewById(R.id.search_book);
         searchBook.setOnClickListener(new TheClickListener());
+        Button addBookToList = findViewById(R.id.add_book_to_list);
+        addBookToList.setOnClickListener(new TheClickListener());
+        addBookToList.setTextColor(0xFFD0EFC6);
+        addBookToList.setEnabled(false);
     }
 
     class TheTextWatcher implements android.text.TextWatcher {
@@ -57,76 +64,80 @@ public class AddBookActivity extends AppCompatActivity {
     }
     class TheClickListener implements View.OnClickListener{
         EditText EditISBN =  findViewById(R.id.Edit_ISBN);
-        String[] keys = new String[]{"书名:          ","作者:          ","出版社:      ","出版年份:   "};
         @Override
         public void onClick(View v){
             if(v.getId()==R.id.search_book) {
                 String ISBN = EditISBN.getText().toString();
-
                 SearchBook(ISBN);
-                String[] values=new String[]{"西游记","吴承恩","人教社","2022"};
+            }else if(v.getId()==R.id.add_book_to_list) {
 
-                ListView listView=findViewById(R.id.listview);
-                List<Map<String,String>> listItems = new ArrayList<>();
-                for(int i=0;i< keys.length;i++){
-                    Map<String,String> map = new HashMap<>();
-                    map.put("key",keys[i]);
-                    map.put("value",values[i]);
-                    listItems.add(map);
-                }
-                SimpleAdapter adapter = new SimpleAdapter(AddBookActivity.this,listItems,R.layout.book_fields,
-                        new String[]{"key","value"},new int[]{R.id.key,R.id.value});
-                listView.setAdapter(adapter);
             }
         }
     }
 
     public void SearchBook(String ISBN){
-
         new Thread(new Runnable() {
-            HttpURLConnection conn = null;
             BufferedReader br = null;
-            String s = "https://api.jike.xyz/situ/book/isbn/" + ISBN+"?apikey="+apikey;
+            String url = "https://api.jike.xyz/situ/book/isbn/" + ISBN + "?apikey=" + apikey;
+
             @Override
             public void run() {
                 try {
-                    URL url = new URL(s);
-                    conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.setReadTimeout(10000);
-                    conn.setConnectTimeout(10000);
-                    conn.setRequestProperty("accept", "*/*");
-                    conn.setRequestProperty("connection", "Keep-Alive");
-                    conn.setRequestProperty("user-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-//                    conn.setDoInput(true);
-                    conn.connect();
-//                    conn.getInputStream();
-//            br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
-//            String buf = null;
-//            buf = br.readLine();
-                    TextView is = findViewById(R.id.Text_ISBN);
-//            is.setText(buf);
-                    is.setText("success");
-
-                }catch (Exception e){
-                    TextView is = findViewById(R.id.Text_ISBN);
-                    is.setText(e.getMessage());
-                }
-                finally {
-                    if (br != null) {
-                        try {
-                            br.close();
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                    HttpReqData req_data = new HttpReqData(url);
+                    HttpRespData resp_data = new HttpRespData();
+                    HttpRequestUtil requestUtil = new HttpRequestUtil();
+                    resp_data = requestUtil.getData(req_data);
+                    JSONObject jsonObject = new JSONObject(resp_data.content);
+                    if(Integer.parseInt(jsonObject.getString("ret"))==0){
+                        String bookName = jsonObject.getJSONObject("data").getString("name");
+                        String author = jsonObject.getJSONObject("data").getString("author");
+                        String publishing = jsonObject.getJSONObject("data").getString("publishing");
+                        String published = jsonObject.getJSONObject("data").getString("published");
+                        String[] keys = new String[]{"书名:          ","作者:          ","出版社:      ","出版年份:   "};
+                        String[] values=new String[]{bookName,author,published,publishing};
+                        ListView listView=findViewById(R.id.listview);
+                        List<Map<String,String>> listItems = new ArrayList<>();
+                        for(int i=0;i< keys.length;i++){
+                            Map<String,String> map = new HashMap<>();
+                            map.put("key",keys[i]);
+                            map.put("value",values[i]);
+                            listItems.add(map);
                         }
+                        SimpleAdapter adapter = new SimpleAdapter(AddBookActivity.this,listItems,R.layout.book_fields,
+                                new String[]{"key","value"},new int[]{R.id.key,R.id.value});
+                        listView.setAdapter(adapter);
+                        Button addBookToList = findViewById(R.id.add_book_to_list);
+                        addBookToList.setTextColor(0xFFFFFFFF);
+                        addBookToList.setEnabled(true);
+                    }else{
+                        String[] keys = new String[]{"error:"};
+                        String message = jsonObject.getString("msg");
+                        String[] values=new String[]{message};
+                        ListView listView=findViewById(R.id.listview);
+                        List<Map<String,String>> listItems = new ArrayList<>();
+                        for(int i=0;i< keys.length;i++){
+                            Map<String,String> map = new HashMap<>();
+                            map.put("key",keys[i]);
+                            map.put("value",values[i]);
+                            listItems.add(map);
+                        }
+                        SimpleAdapter adapter = new SimpleAdapter(AddBookActivity.this,listItems,R.layout.book_fields,
+                                new String[]{"key","value"},new int[]{R.id.key,R.id.value});
+                        listView.setAdapter(adapter);
+                        Button addBookToList = findViewById(R.id.add_book_to_list);
+                        addBookToList.setTextColor(0xFFD0EFC6);
+                        addBookToList.setEnabled(false);
                     }
-                    if (conn != null) {
-                        conn.disconnect();
-                    }
-                }
+
+
+                } catch (Exception e) {
+
+                } finally {
+
                 }
 
-            }).start();
+            }
+        }).start();
 
 
     }
